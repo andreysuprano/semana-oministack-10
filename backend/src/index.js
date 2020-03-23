691c065a6d6c967dbfd44a3cf23023b425fe2ed3
Andrey Suprano <andsuprano@gmail.com>
2020-03-23 09:56:45 -0300

    Subindo aquivos de backend
    Existem alguns bugs no mobile1

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://andrey:andrey@cluster0-yw1nu.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
